040c9b991108b45b0aab8cb79a9ddc49729cb559
Morgan Rees <morgan.rees@dotdigital.com>
2023-03-23 12:46:22 +0000

    Add project files.

---
+++ 40kScraper.Console/MiniatureDetails.cs
@@ -0,0 +1,11 @@
+﻿public class MiniatureDetails
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string[] ImageUrls { get; set; }
+
+    public override string? ToString()
+    {
+        return $"{Name}, Url: {Url}, Images: {string.Join(',', ImageUrls)}";
+    }
+}
---
+++ 40kScraper.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using PuppeteerSharp;
+using System.Globalization;
+
+var url = "URL HERE";
+
+await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = true
+});
+
+var page = await browser.NewPageAsync();
+
+await page.SetUserAgentAsync("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
+
+await page.GoToAsync(url);
+
+await page.WaitForSelectorAsync("h1");
+
+var content = await page.GetContentAsync();
+
+var doc = new HtmlDocument();
+doc.LoadHtml(content);
+
+var docImages = doc.DocumentNode
+    .Descendants("img")
+    .Where(x => x.HasClass("media-image"))
+    .SelectMany(x => x.Attributes
+        .Where(a => a.Name == "data-lazy")
+        .Select(a => new { Image = a.Value }))
+    .ToList();
+
+using (var writer = new StreamWriter("ork-images.csv"))
+using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(docImages);
+}
